6ddf657838e317ab7f124b66cb16886ef784a695
Alex Tay <alextaymx@gmail.com>
2023-05-15 15:31:28 +0800

    minor adjustment

--- src/components/custom-layout/right-panel/RightPanel.tsx
+++ src/components/custom-layout/right-panel/RightPanel.tsx
@@ -1,12 +1,13 @@
-import RevenueGroupList from '@/components/revenue-group/table-list/RevenueGroupList';
 import React from 'react';
+
+import RevenueGroupList from '@/components/revenue-group/table-list/RevenueGroupList';
 
 type Props = {};
 
 function RightPanel(props: Props) {
   return (
     <div className='w-full h-full col-span-2 border border-transparent rounded'>
-      <div className='text-lg'>Browse Revenue Groups</div>
+      <div className='text-lg mb-5'>Browse Revenue Groups</div>
 
       <RevenueGroupList />
     </div>
