a0d25aadfdb0eb07f34c9c84421ffe78a8d7937c
Alex Tay <alextaymx@gmail.com>
2023-05-15 11:47:55 +0800

    starter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  eslint: {
+    dirs: ["."],
+  },
+
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: "",
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
